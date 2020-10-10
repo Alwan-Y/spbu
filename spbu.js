e9a1ae0156c0cbdd3756f2675908f86aca0462d4
Alwan Yusuf <aalyusuf12@gmail.com>
2020-10-10 23:25:43 +0700

    50%

--- spbu.js
+++ spbu.js
@@ -1,10 +1,10 @@
 class Station {
     constructor(name) {
         this.name = name;
-        this.shelter = 100;
+        this.shelter = 1000;
     }
     setName = name => this.name = name;
-    setShelter = amount => this.shelter += amount;
+    setShelter = fuel => this.shelter += fuel;
 
     getName = () => {
         return this.name;
@@ -14,7 +14,7 @@ class Station {
     }
 
     chargingWarning = () => {
-        if (this.shelter < 10) {
+        if (this.shelter < 50) {
             console.log(`remaining fuel is too little, please ask to be filled`)
         } else {
             console.log(`there is still a lot of fuel left`)
@@ -64,9 +64,12 @@ class Employee extends person {
         super(name)
         this.employee = false;
         this.transactionList = [];
+        this.CASH = 0;
+        // this.nameListTransaction = [];
     }
     setEmployee = boole => this.employeeList = boole;
     setTransactionList = transaction => this.transactionList.push(transaction);
+    setCash = cash => this.CASH = cash;
 
     getEmployee = () => {
         return this.employeeList;
@@ -74,21 +77,41 @@ class Employee extends person {
     getTransactionList = () => {
         return this.transactionList;
     }
+    getCash = () => {
+        return this.CASH;
+    }
+
+    refuel = (customer, fuel, station) => {
+        if(this.getEmployee()){
+            if(station.getShelter() > fuel) {
+                if(customer.getTankCapacity() > fuel) {
+                    customer.SetTankFilled(fuel)
+                    customer.setPurchaseTime()
+                    this.setTransactionList([customer,fuel])
+                    station.setShelter(-fuel)
+                } else {
+                    console.log(`the vehicle fuel tank is not enough, please reduce your purchase`)
+                }
+            } else {
+                console.log(`fuel is not enough, please order a refueling station`)
+            }
+        } else {
+            console.log(`you are not a worker !!, please contact the owner first`)
+        }
+    }    
 }
 
 class Customer extends Person {
-    constructor(name, origin, tankCapacity, amount) {
+    constructor(name, origin, tankCapacity) {
         super(name, origin)
         this.member = false;
         this.PURCHASE_TIMES = 0
         this.tankCapacity = tankCapacity;
-        this.amount = amount;
         this.tankFilled = 1; // karena gamungkin pas ngisi itu kosong
     }
     setMember = member => this.member = member;
-    setPurchaseList = () => this.PURCHASE_TIMES++;
+    setPurchaseTime = () => this.PURCHASE_TIMES++;
     setTankCapacity = capacity => this.tankCapacity = capacity;
-    setAmount = amount => this.amount = amount;
     SetTankFilled = capacity => this.SetTankFilled += capacity
 
     getName = () => {
@@ -97,16 +120,15 @@ class Customer extends Person {
     getMembber = () => {
         return this.member;
     }
-    getPurchaseList = () => {
+    getPurchaseTimes = () => {
         return this.PURCHASE_TIMES;
     }
     getTankCapacity = () => {
         return this.tankCapacity;
     }
-    getAmount = () => {
-        return this.amount;
-    }
     getTankFilled = () => {
         return this.tankFilled;
     }
 }
+
+const station = new Station('station 1')
